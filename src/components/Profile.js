30447e68d4009780197b52f644069ebdd54b0cf7
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-27 14:18:45 +0100

    Change profile image to Slack pfp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import profileImage from "../images/profile-image.jpg";
+import profileImage from "../images/profile-img.jpg";
 
 export default function Profile() {
   return (
     <div>
-      <img src={profileImage} alt="Kelechi Nwa-uwa's profile" />
+      <img src={profileImage} alt="Kelechi Nwa-uwa's profile" id="profile__image" />
       <p>Kelechi Nwa-uwa</p>
     </div>
   )
