4445cc5bf20c141e82169e4907f2f7837efebac4
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-28 11:47:59 +0100

    Add extra links to list and import styling

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Link from "./Link.js";
+import "../styles/LinkList.css";
+import SlackLogo from "./logo-svg-components/SlackLogo.js";
+import GithubLogo from "./logo-svg-components/GithubLogo.js";
 
 export default function LinkList() {
   const links = [
+    {
+      id: "twitter",
+      url: "https://twitter.com/theAvocadoCoder/",
+      text: "Twitter Link",
+    },
+    {
+      id: "slack",
+      url: "https://slack.com/theAvocadoCoder/",
+      text: "Slack",
+    },
     {
       id: "btn__zuri",
       url: "https://training.zuri.team/",
@@ -31,8 +44,16 @@ export default function LinkList() {
   ]
 
   return (
-      links.map(link => {
-        return <Link key={link.id} link={link} />
-      })
+    <div id="links__div">
+      { 
+        links.map(link => {
+          return <Link key={link.id} link={link} />
+        }) 
+      }
+      <div id="extra__div">
+        <SlackLogo />
+        <GithubLogo />
+      </div>
+    </div>
   )
 }
