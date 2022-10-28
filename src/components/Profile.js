f3ee2211c2db69fcd35f8c8307e6125479803a73
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-28 11:56:37 +0100

    Conditionally render share icon based on device screen

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import profileImage from "../images/profile-img.jpg";
 import "../styles/Profile.css";
+import MoreDetailsIcon from './icon-svg-components/MoreDetailsIcon';
+import ShareIcon from './icon-svg-components/ShareIcon';
 
 export default function Profile() {
   return (
     <div>
       <img src={profileImage} alt="Kelechi Nwa-uwa's profile" id="profile__image" />
-      <p>Kelechi Nwa-uwa</p>
+      <p id="profile__name">Kelechi Nwa-uwa</p>
+      <span id="share__btn">
+        { isNotMobile ? <ShareIcon /> : <MoreDetailsIcon />}
+      </span>
     </div>
   )
 }
