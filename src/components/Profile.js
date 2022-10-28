b597b328852e8bd19058449f723d003d191758b6
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-28 11:57:34 +0100

    Include media query hook for conditional rendering of share icon

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useMediaQuery from "../hooks/useMediaQuery";
 import profileImage from "../images/profile-img.jpg";
 import "../styles/Profile.css";
 import MoreDetailsIcon from './icon-svg-components/MoreDetailsIcon';
 import ShareIcon from './icon-svg-components/ShareIcon';
 
 export default function Profile() {
+  const isNotMobile = useMediaQuery("(min-width: 1024px)");
   return (
     <div>
       <img src={profileImage} alt="Kelechi Nwa-uwa's profile" id="profile__image" />
