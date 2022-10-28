d7076cc294e0bc159d6e540c7c083f0c6d8718a0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-10-28 11:53:27 +0100

    Import Footer component and App style sheet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Profile from "./components/Profile.js";
 import LinkList from "./components/LinkList.js";
+import Footer from "./components/Footer.js";
+import "./styles/App.css";
 
 function App() {
   return (
-    <>
+    <div id="app__div">
       <Profile />
       <LinkList />
-    </>
+      <Footer />
+    </div>
   );
 }
 
