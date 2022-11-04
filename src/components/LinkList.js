17de2e47fc8597ea8a20c86a03ae3ca7a70077f7
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-04 13:02:26 +0100

    refactor: Move links array to separate module

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,49 +3,9 @@ import Link from "./Link.js";
 import "../styles/LinkList.css";
 import SlackLogo from "./logo-svg-components/SlackLogo.js";
 import GithubLogo from "./logo-svg-components/GithubLogo.js";
+import { links } from "../links.js";
 
 export default function LinkList() {
-  const links = [
-    {
-      id: "twitter",
-      url: "https://twitter.com/theAvocadoCoder/",
-      text: "@theAvocadoCoder [Twitter]",
-    },
-    {
-      id: "slack",
-      url: "https://hng9.slack.com/team/U048NMSEX6D/",
-      text: "theAvocadoCoder",
-    },
-    {
-      id: "btn__zuri",
-      url: "https://training.zuri.team/",
-      text: "Zuri Team",
-    },
-    {
-      id: "books",
-      url: "https://books.zuri/team/",
-      text: "Zuri Books",
-      subtext: "Look through our super helpful catalog of coding and design books",
-    },
-    {
-      id: "book__python",
-      url: "https://books.zuri.team/python-for-beginners?ref_id=theAvocadoCoder",
-      text: "Python Books",
-      subtext: `Are you a code newbie?\nLooking to solidify your understanding of python and be the next big name in machine learning?\nLook no further; our Python for Beginners book is the perfect place to start you on your journey!`,
-    },
-    {
-      id: "pitch",
-      url: "https://backgroundcheck.zuri.team/",
-      text: "Background Check for Coders",
-      subtext: "Learn about the good, bad, and downright scary online activities of candidates and employees.\nLet us help you make better informed hiring decisions.",
-    },
-    {
-      id: "book__design",
-      url: "https://books.zuri.team/design-rules",
-      text: "Design Books",
-      subtext: "The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.",
-    },
-  ]
 
   return (
     <div id="links__div">
