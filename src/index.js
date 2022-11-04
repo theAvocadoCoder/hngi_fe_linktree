57096da9faf014d77f610aa4eca8c50ef5318ec7
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-04 14:36:53 +0100

    feat: Render Contact component at path

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/normalize.css";
 import App from './App.js';
+import Contact from './routes/Contact';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
 ],
 {
   basename: "/hngi_fe_linktree"
