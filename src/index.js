8a9f3fee7f667475446c37c1fa4fe2412ef855f1
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-04 13:30:28 +0100

    chore: Create and render browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/normalize.css";
-import App from './App';
+import App from './App.js';
+
+const router = createBrowserRouter([
+  {
+    path: "/hngi_fe_linktree",
+    element: <App />,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
