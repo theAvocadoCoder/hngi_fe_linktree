c589a0867bfe6241c86565afc611d0e0aef03b26
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2022-11-05 11:56:18 +0100

    refactor: Properly position Contact elements

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -6,67 +6,68 @@ import "../styles/Contact.css";
 export default function Contact() {
   return (
     <div id="contact__div">
-    <Form method="post" id="contact__form">
-      <h1>Contact Me</h1>
-      <p>Hi there, contact me to ask about anything you have in mind.</p>
-      <div id="form_fields__div">
-        <div id="names__div">
-          <label htmlFor="first_name">
-            <span>First name</span>
+      <div>
+        <h1>Contact Me</h1>
+        <p>Hi there, contact me to ask about anything you have in mind.</p>
+      </div>
+      <Form method="post" id="contact__form">
+        <div id="form_fields__div">
+          <div id="names__div">
+            <label htmlFor="first_name">
+              <span>First name</span>
+              <input 
+                type="text" 
+                name="first_name" 
+                id="first_name" 
+                placeholder="Enter your first name" 
+                aria-label="First Name" 
+              />
+            </label>
+            <label htmlFor="last_name">
+              <span>Last name</span>
+              <input 
+                type="text" 
+                name="last_name" 
+                id="last_name" 
+                placeholder="Enter your last name" 
+                aria-label="Last Name" 
+              />
+            </label>
+          </div>
+          <label htmlFor="email">
+            <span>Email</span>
             <input 
-              type="text" 
-              name="first_name" 
-              id="first_name" 
-              placeholder="Enter your first name" 
-              aria-label="First Name" 
+              type="email" 
+              name="email" 
+              id="email" 
+              placeholder="yourname@email.com" 
+              aria-label="Email" 
             />
           </label>
-          <label htmlFor="last_name">
-            <span>Last name</span>
-            <input 
-              type="text" 
-              name="last_name" 
-              id="last_name" 
-              placeholder="Enter your last name" 
-              aria-label="Last Name" 
+          <label htmlFor="message">
+            <span>Message</span>
+            <textarea
+              name="message" 
+              id="message" 
+              placeholder="Send me a message and I'll reply you as soon as possible..." 
+              aria-label="message" 
             />
+          </label> 
+          <label htmlFor="consent__checkbox">
+            <input 
+              type="checkbox" 
+              name="consent__checkbox" 
+              id="consent__checkbox"
+              aria-label="Consent Checkbox" 
+            />
+            <p>You agree to providing your data to Kelechi who may contact you</p>
           </label>
-        </div>
-        <label htmlFor="email">
-          <span>Email</span>
-          <input 
-            type="email" 
-            name="email" 
-            id="email" 
-            placeholder="yourname@email.com" 
-            aria-label="Email" 
-          />
-        </label>
-        <label htmlFor="message">
-          <span>Message</span>
-          <textarea
-            name="message" 
-            id="message" 
-            placeholder="Send me a message and I'll reply you as soon as possible..." 
-            aria-label="message" 
-          />
-        </label> 
-        <label htmlFor="consent__checkbox">
-          <input 
-            type="checkbox" 
-            name="first_name" 
-            id="first_name" 
-            placeholder="FirstName" 
-            aria-label="First Name" 
-          />
-          <span>You agree to providing your data to Kelechi who may contact you</span>
-        </label>
-        <label htmlFor="btn__submit">
-          <button type="submit">Send Message</button>
-        </label>
-      </div> 
-    </Form>
-    <Footer />
+          <label htmlFor="btn__submit">
+            <button type="submit" id="btn__submit">Send Message</button>
+          </label>
+        </div> 
+      </Form>
+      <Footer />
     </div>
   )
 }
